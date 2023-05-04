0df53169a3fab85a37ffa0e0cb8b8ae52a10290e
jsp92 <jackson.pace.2@gmail.com>
2023-05-04 09:56:53 -0400

    initial commit 	new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   test/pageobjects/login.page.js 	new file:   test/pageobjects/page.js 	new file:   test/pageobjects/secure.page.js 	new file:   test/specs/example.e2e.js 	new file:   wdio.conf.js

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,53 @@
+
+describe('QA Interview Factorial Webpage', () => {
+    beforeEach(async() => {
+        await browser.url("https://qainterview.pythonanywhere.com/");
+    });
+    
+    /*
+    it('Happy Path', async () => {
+        await $("#number").setValue(5);
+        await $("#getFactorial").click();
+        await browser.pause(3000);
+    });
+*/
+
+/*
+    it('Validate Test Fails when Finding Factorial of Letters', async () => {
+        await $("#number").setValue(abcdefg);
+        await $("#getFactorial").click();
+        await browser.pause(3000);
+    });
+
+    */
+
+
+    //test fails as expected
+
+/*
+    it('Validate Test Fails when Finding Factorial of Japanese Characters', async () => {
+        await $("#number").setValue(こんにちは世界);
+        //Hello World in Japanese 
+        await $("#getFactorial").click();
+        await browser.pause(3000);
+    });
+*/
+
+//test fails as expected
+
+/*
+
+it('Validate Test Fails when Finding Factorial of Negative Number', async () => {
+    await $("#number").setValue(-5);
+    await $("#getFactorial").click();
+    await browser.pause(3000);
+});
+
+*/
+
+//Number is entered into text box.  
+//Test passes, but button is not clicked...
+
+});
+
+
